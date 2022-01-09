c7e01e456517ae2692b611a984f1b18de1f6adf8
Sanhamm <sanhamm097@gmail.com>
2022-01-09 20:32:46 +0100

    MA Ferdig

--- style.js
+++ style.js
@@ -1,1 +1,55 @@
+//Ma Programming Foundations
+
 console.log("MA av Sander Hammersland");
+console.log(" ");
+// Task 1
+console.log("task 1:");
+var number = 3;
+console.log(number);
+var booly = true
+console.log(booly);
+var string = "string"
+console.log(string);
+console.log(" ");
+
+
+
+
+//task 2 
+console.log("Task 2:");
+var fname = "Sander";
+var lname = "Hammersland";
+var full = (fname + " " + lname);
+console.log(full);
+console.log(" ");
+
+
+
+
+//task 3 
+console.log("Task 3:");
+var o = "frog"
+console.log ("The type of frog is 'string' ")
+
+
+console.log(" ");
+//task 4
+console.log("Task 4:");
+var orderHasShiped = true;
+
+if (orderHasShiped === true) {
+    say = "The order shipped"
+}
+else {
+    say = "The order did not ship"
+}
+console.log(say);
+console.log(" ");
+
+
+//task 5
+
+console.log("Task 5:");
+for(var i = 7; i <= 13; i++) {
+    console.log("The number is: " + i);
+}
